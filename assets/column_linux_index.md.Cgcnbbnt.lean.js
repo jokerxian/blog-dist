c06843b5aa22e35276360ea3c7c28dc4b9e9ae28
xianw <wangxian@longshine.com>
2024-04-09 12:56:30 +0800

    0409静态dist

---
+++ assets/column_linux_index.md.Cgcnbbnt.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as n,o as t}from"./chunks/framework.D3nF4eGI.js";const p=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"column/linux/index.md","filePath":"column/linux/index.md"}'),a={name:"column/linux/index.md"};function o(c,r,i,s,d,l){return t(),n("div")}const _=e(a,[["render",o]]);export{p as __pageData,_ as default};
