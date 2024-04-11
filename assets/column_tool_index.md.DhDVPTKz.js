228c0410da190f82f538204a10bdbc9d2faa1743
xianw <wangxian@longshine.com>
2024-04-11 15:15:49 +0800

    Auto commit by script

---
+++ assets/column_tool_index.md.DhDVPTKz.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./chunks/framework.BgExJILf.js";const p=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"column/tool/index.md","filePath":"column/tool/index.md"}'),n={name:"column/tool/index.md"};function a(c,r,s,d,i,l){return o(),t("div")}const _=e(n,[["render",a]]);export{p as __pageData,_ as default};
